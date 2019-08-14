37045edd2dc87a105af5da1db4fc26476f3cd94a
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-08-14 15:23:46 -0300

    :sparkles: Verificando se o jogador ganhou

--- src/views/BingoCard.vue
+++ src/views/BingoCard.vue
@@ -1,7 +1,9 @@
 <template>
   <VRow align="center">
     <VCol class="text-center display-1" cols="12">
+      <span v-if="verifyCard">🎉</span>
       Cartela
+      <span v-if="verifyCard">🎉</span>
     </VCol>
     <VCol cols="12">
       <VCard class="py-5 text-center">
@@ -20,9 +22,8 @@
               class="pa-2 text-center"
               :color="num.selected ? 'grey lighten-1' : ''"
               @click="num.selected = !num.selected"
+              >{{ num.value }}</VCard
             >
-              {{ num.value }}
-            </VCard>
           </VCol>
         </VRow>
       </VCard>
@@ -39,6 +40,24 @@ export default {
     bingoCard: undefined,
     borders: [0, 5, 10, 15, 20]
   }),
+  computed: {
+    verifyCard() {
+      const selecteds = this.bingoCard.map(item => item.selected);
+      const lines = [
+        selecteds.splice(0, 5),
+        selecteds.splice(0, 5),
+        selecteds.splice(0, 5),
+        selecteds.splice(0, 5),
+        selecteds.splice(0, 5)
+      ];
+
+      if (this.verifyLines(lines)) return true;
+      if (this.verifyColumns(lines)) return true;
+      if (this.verifyDiagonal(lines)) return true;
+
+      return false;
+    }
+  },
   created() {
     const numList = this.shuffle();
 
@@ -55,9 +74,46 @@ export default {
       }
 
       list = list.sort();
-      list.splice(12, 0, { value: 'BINGO', selected: false });
+      list.splice(12, 0, { value: 'BINGO', selected: true });
 
       return list;
+    },
+    verifyLines(lines) {
+      let hasWon = false;
+
+      lines.forEach(line => {
+        if (line.indexOf(false) === -1) hasWon = true;
+      });
+
+      return hasWon;
+    },
+    verifyColumns(lines) {
+      const columns = [];
+      for (let i = 0; i < 5; i += 1) {
+        const column = [];
+        lines.forEach(line => {
+          column.push(line[i]);
+        });
+
+        columns.push(column);
+      }
+
+      if (this.verifyLines(columns)) return true;
+      return false;
+    },
+    verifyDiagonal(lines) {
+      const diagonals = [[], []];
+
+      let inverse = 4;
+      for (let i = 0; i < 5; i += 1) {
+        diagonals[0].push(lines[i][i]);
+        diagonals[1].push(lines[i][inverse]);
+
+        inverse -= 1;
+      }
+
+      if (this.verifyLines(diagonals)) return true;
+      return false;
     }
   }
 };
